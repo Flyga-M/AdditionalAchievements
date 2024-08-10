70916ab4566f580619c0a7555891b358cb255b1d
Yannick Yannick <yannickjanssen@gmx.de>
2024-08-10 20:10:39 +0200

    updated AchievementView to make it more flexible amd reusable - added and implemented AchievementPresenter

---
+++ UI/Presenters/_WindowView/AchievementPresenter.cs
@@ -0,0 +1,44 @@
+﻿using AchievementLib.Pack;
+using Blish_HUD.Graphics.UI;
+using Flyga.AdditionalAchievements.UI.Controls;
+using Flyga.AdditionalAchievements.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyga.AdditionalAchievements.UI.Presenters
+{
+    public class AchievementPresenter : Presenter<AchievementView, IAchievement>
+    {
+        public AchievementPresenter(AchievementView view, IAchievement model) : base(view, model)
+        {
+            View.ParentResized += OnViewParentResized;
+        }
+
+        private void OnViewParentResized(object _, EventArgs _1)
+        {
+            if (View.AchievementContent != null && View.AchievementContent is AchievementDescription description)
+            {
+                description.Height = description.GetActualHeight() + 30;
+            }
+        }
+
+        protected override void UpdateView()
+        {
+            // will be disposed by the view, so they don't need to be disposed here
+            View.ProgressIndicator = new AchievementProgressSquare(Model, true);
+            
+            AchievementDescription description = new AchievementDescription(Model);
+            View.AchievementContent = description;
+            description.Height = description.GetActualHeight() + 30;
+
+        }
+
+        protected override void Unload()
+        {
+            View.ParentResized -= OnViewParentResized;
+        }
+    }
+}
--- UI/Views/_WindowView/AchievementView.cs
+++ UI/Views/_WindowView/AchievementView.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using AchievementLib.Pack;
+﻿using AchievementLib.Pack;
 using Blish_HUD;
 using Blish_HUD.Controls;
 using Blish_HUD.Graphics.UI;
@@ -8,7 +7,9 @@ using Flyga.AdditionalAchievements.Textures;
 using Flyga.AdditionalAchievements.Textures.Colors;
 using Flyga.AdditionalAchievements.UI.Controls;
 using Flyga.AdditionalAchievements.UI.Models;
+using Flyga.AdditionalAchievements.UI.Presenters;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace Flyga.AdditionalAchievements.UI.Views
 {
@@ -16,7 +17,7 @@ namespace Flyga.AdditionalAchievements.UI.Views
     {
         private static readonly Logger Logger = Logger.GetLogger<AchievementView>();
 
-        private IAchievement _achievement;
+        //private IAchievement _achievement;
         private BackData _backData;
 
         private Container _parent;
@@ -31,23 +32,125 @@ namespace Flyga.AdditionalAchievements.UI.Views
 
         private const int _paddingColumns = 10;
 
-        private AchievementProgressSquare _progressSquare;
-        private AchievementDescription _description;
+        private Control _progressIndicator;
+        private Control _achievementContent;
+
+        private bool _showCompletedHighlight;
+        private Color? _completedHighlightColor;
+
+
+        //private AchievementProgressSquare _progressSquare;
+        //private AchievementDescription _description;
+
+        /// <summary>
+        /// The progress indicator.
+        /// </summary>
+        /// <remarks>
+        /// Disposes the previous <see cref="ProgressIndicator"/>, if it's not <see langword="null"/> and overwritten. 
+        /// Will also be disposed, when the <see cref="AchievementView"/> is unloaded.
+        /// </remarks>
+        public Control ProgressIndicator
+        {
+            get => _progressIndicator;
+            set
+            {
+                if (_progressIndicator != null)
+                {
+                    _progressIndicator.Parent = null;
+                    _progressIndicator.Dispose();
+                }
+
+                _progressIndicator = value;
+
+                if (_leftColumn != null)
+                {
+                    _progressIndicator.Parent = _leftColumn;
+                    _progressIndicator.Width = _leftColumn.Width;
+                    _progressIndicator.Height = _leftColumn.Width;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The content of the achievement. Usually contains the title, progress (in text form) and a description of 
+        /// the achievement.
+        /// </summary>
+        /// <remarks>
+        /// Disposes the previous <see cref="AchievementContent"/>, if it's not <see langword="null"/> and overwritten. 
+        /// Will also be disposed, when the <see cref="AchievementView"/> is unloaded.
+        /// </remarks>
+        public Control AchievementContent
+        {
+            get => _achievementContent;
+            set
+            {
+                if (_achievementContent != null)
+                {
+                    _achievementContent.Parent = null;
+                    _achievementContent.Dispose();
+                }
+
+                _achievementContent = value;
+
+                if (_rightColumn != null)
+                {
+                    _achievementContent.Parent = _rightColumn;
+                    _achievementContent.Width = _rightColumn.Width;
+                    _achievementContent.Height = _rightColumn.Height;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the background highlight that usually marks a completed achievement, will 
+        /// be visible.
+        /// </summary>
+        public bool ShowCompletedHighlight
+        {
+            get => _showCompletedHighlight;
+            set
+            {
+                _showCompletedHighlight = value;
+                if (_achievementCompletedHighlight != null)
+                {
+                    _achievementCompletedHighlight.Visible = value;
+                }
+            }
+        }
+
+        public Color CompletedHighlightColor
+        {
+            get => _completedHighlightColor ?? ColorManager.AchievementFallbackColor;
+            set
+            {
+                _completedHighlightColor = value;
+                if (_achievementCompletedHighlight != null)
+                {
+                    _achievementCompletedHighlight.Tint = CompletedHighlightColor * 0.5f;
+                }
+            }
+        }
 
         /// <summary>
         /// Fires, when the <see cref="BackBar"/> was clicked.
         /// </summary>
         public event EventHandler Back;
 
+        public event EventHandler ParentResized;
+
         private void OnBack()
         {
             Back?.Invoke(this, EventArgs.Empty);
         }
 
-        public AchievementView(IAchievement achievement, BackData backData)
+        public AchievementView(BackData backData)
         {
-            _achievement = achievement;
             _backData = backData;
+        }
+
+        public AchievementView(IAchievement achievement, BackData backData) : this (backData)
+        {
+            this.WithPresenter(new AchievementPresenter(this, achievement));
         }
 
         private void OnBackBarClick(object _, MouseEventArgs _1)
@@ -78,10 +181,10 @@ namespace Flyga.AdditionalAchievements.UI.Views
                     _leftColumn.Height = _content.Height;
                     _leftColumn.Width = _content.Width / 5;
 
-                    if (_progressSquare != null)
+                    if (_progressIndicator != null)
                     {
-                        _progressSquare.Width = _leftColumn.ContentRegion.Width;
-                        _progressSquare.Height = _leftColumn.ContentRegion.Width;
+                        _progressIndicator.Width = _leftColumn.ContentRegion.Width;
+                        _progressIndicator.Height = _leftColumn.ContentRegion.Width;
                     }
                 }
 
@@ -91,10 +194,11 @@ namespace Flyga.AdditionalAchievements.UI.Views
                     _rightColumn.Width = _content.Width - (_leftColumn.Width + _paddingColumns);
                     _rightColumn.Left = _leftColumn.Width + _paddingColumns;
 
-                    if (_description != null)
+                    if (_achievementContent != null)
                     {
-                        _description.Width = _rightColumn.Width;
-                        _description.Height = _description.GetActualHeight() + 20;
+                        _achievementContent.Width = _rightColumn.Width;
+                        // TODO: fix
+                        _achievementContent.Height = _rightColumn.Height;
                     }
                 }
             }
@@ -110,6 +214,7 @@ namespace Flyga.AdditionalAchievements.UI.Views
         private void OnParentResized(object _, ResizedEventArgs _1)
         {
             RecalculateLayout();
+            ParentResized?.Invoke(this, EventArgs.Empty);
         }
 
         protected override void Build(Container buildPanel)
@@ -141,11 +246,12 @@ namespace Flyga.AdditionalAchievements.UI.Views
                 ClipsBounds = false
             };
 
-            _progressSquare = new AchievementProgressSquare(_achievement, true);
-
-            _progressSquare.Parent = leftColumn;
-            _progressSquare.Width = leftColumn.ContentRegion.Width;
-            _progressSquare.Height = leftColumn.ContentRegion.Width;
+            if (_progressIndicator != null)
+            {
+                _progressIndicator.Parent = leftColumn;
+                _progressIndicator.Width = leftColumn.Width;
+                _progressIndicator.Height = leftColumn.Width;
+            }
 
             return leftColumn;
         }
@@ -161,41 +267,29 @@ namespace Flyga.AdditionalAchievements.UI.Views
                 CanScroll = true
             };
 
-            try
+            if (_achievementContent != null)
             {
-                _description = new AchievementDescription(_achievement)
-                {
-                    Parent = rightColumn,
-                    Width = rightColumn.Width,
-                    Height = rightColumn.Height
-                };
-
-                _description.Height = _description.GetActualHeight() + 20;
+                _achievementContent.Parent = rightColumn;
+                _achievementContent.Width = rightColumn.Width;
+                _achievementContent.Height = rightColumn.Height;
             }
-            catch (Exception ex)
-            {
-                Logger.Warn($"Ayy: {ex}");
-            }
-            
 
             return rightColumn;
         }
 
         private Panel BuildContent(Container buildPanel)
         {
-            if (_achievement.IsFulfilled)
+            _achievementCompletedHighlight = new Image()
             {
-                _achievementCompletedHighlight = new Image()
-                {
-                    Parent = buildPanel,
-                    Texture = TextureManager.Display.Description.CompletedBackgroundHighlight,
-                    ClipsBounds = false,
-                    Location = new Point(0, _backBar.Height),
-                    Height = buildPanel.Height - _backBar.Height,
-                    Width = buildPanel.Width,
-                    Tint = (_achievement.Color ?? ColorManager.AchievementFallbackColor) * 0.5f
-                };
-            }
+                Parent = buildPanel,
+                Texture = TextureManager.Display.Description.CompletedBackgroundHighlight,
+                ClipsBounds = false,
+                Location = new Point(0, _backBar.Height),
+                Height = buildPanel.Height - _backBar.Height,
+                Width = buildPanel.Width,
+                Visible = ShowCompletedHighlight,
+                Tint = CompletedHighlightColor * 0.5f
+            };
 
             Panel contentContainer = new Panel()
             {
@@ -221,16 +315,16 @@ namespace Flyga.AdditionalAchievements.UI.Views
                 _parent.Resized -= OnParentResized;
             }
             
-            if (_progressSquare != null)
+            if (_progressIndicator != null)
             {
-                _progressSquare.Dispose();
-                _progressSquare = null;
+                _progressIndicator.Dispose();
+                _progressIndicator = null;
             }
 
-            if (_description != null)
+            if (_achievementContent != null)
             {
-                _description.Dispose();
-                _description = null;
+                _achievementContent.Dispose();
+                _achievementContent = null;
             }
 
             if (_achievementCompletedHighlight != null)
